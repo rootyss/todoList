33fa415bbe6c82a441f13add5013d2bd27d485e1
Vladimir Krivshuk <krivshuk@ispras.ru>
2022-12-26 16:24:41 +0300

    added server state

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import { uniqueId } from "lodash";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -6,12 +7,31 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/api/count", (req, res) => {
-  res.json({ count: 1 });
+type Todo = {
+  id: string;
+  name: string;
+  author: string;
+};
+
+export type Db = {
+  toDos: Todo[];
+};
+
+const db: Db = {
+  toDos: [{ id: "0", name: "new-todo", author: "rooty" }],
+};
+
+app.use(express.json());
+
+app.get("/api/todos", (req, res) => {
+  res.json(db);
 });
 
-app.post("/count", (req, res) => {
-  console.log(req);
+app.post("/api/todos/add", (req, res) => {
+  const newId = uniqueId();
+  const { name, author } = req.body;
+  db.toDos.push({ id: newId, name, author });
+  res.status(200).json({ requestBody: newId });
 });
 
 // eslint-disable-next-line no-console
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
 import React, { useEffect, useState } from "react";
 
+import { Db } from "../server";
+
 import "./App.css";
 
 const App = (): JSX.Element => {
-  const [counts, setCounts] = useState(0);
+  const [todos, setToDos] = useState<Db>();
+
+  const [newname, setNewName] = useState("");
+  const [newauthor, setNewAuthor] = useState("");
+
+  const [newId, setNewId] = useState("");
 
   useEffect(() => {
-    fetch("/api/count", {
-      method: "GET",
-    }).then((res) => res.json().then(({ count }) => setCounts(count)));
-  }, []);
+    fetch("/api/todos").then((res) =>
+      res.json().then((db: Db) => setToDos(db)),
+    );
+  }, [newId]);
+
+  const onSubmit = async (): Promise<void> => {
+    const data = await fetch("/api/todos/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ name: newname, author: newauthor }),
+    });
+    setNewId(await data.json());
+  };
 
   return (
-    <div className="App">
-      {counts}
-      <form method="post" action="">
-        <input type="number" name="add" />
-        <input type="submit" value="Отправить" />
-      </form>
-    </div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Название</th>
+            <th>Автор</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos?.toDos.map(({ name, id, author }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{author}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="App">
+        <form
+          method="post"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit();
+          }}
+        >
+          <input
+            name="newname"
+            value={newname}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <input
+            name="newauthor"
+            value={newauthor}
+            onChange={(e) => setNewAuthor(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="Отправить"
+            onClick={(e) => {
+              e.preventDefault();
+              onSubmit();
+            }}
+          />
+        </form>
+      </div>
+    </>
   );
 };
 
